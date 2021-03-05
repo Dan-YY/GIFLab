c711bdf6e050d18110dde1870cdae594fc6656d0
Dan <dan@dansstudio.ca>
2021-03-05 15:40:40 -0800

    Initial Commit

---
+++ GIFLab/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GIFLab
+//
+//  Created by macbook on 2021-03-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
